e06beb993582ece6bcf6d7e702cc78738d65be55
Helen Ye <00yhelen@gmail.com>
2017-12-19 11:35:15 -0500

    Uh what is this i can't even

--- forking_server.c
+++ forking_server.c
@@ -2,7 +2,8 @@
 #include <signal.h>
 
 void process(char *s);
-void subserver(int from_client);
+// Sorry mr dw we modified this forgive us
+void subserver(int from_client, int to_client);
 
 static void sighandler(int signo) {
     if (signo == SIGINT) {
@@ -21,7 +22,7 @@ int main() {
         int f = fork();
         if (f == 0) {
             to_client = server_connect(from_client);
-            /* subserver(from_client); */
+            subserver(from_client, to_client);
 
             exit(0);
         }
@@ -29,8 +30,8 @@ int main() {
     return 0;
 }
 
-void subserver(int from_client) {
-    char buffer[MESSAGE_BUFFER_SIZE];
+void subserver(int from_client, int to_client) {
+    char buffer[BUFFER_SIZE];
     while (read(from_client, buffer, sizeof(buffer))) {
         printf("[SERVER %d] received: %s\n", getpid(), buffer);
         process(buffer);
@@ -38,11 +39,10 @@ void subserver(int from_client) {
     }
 }
 
-void process(char * s) {
+void process(char * str) {
     unsigned char *my_str = (unsigned char *) str;
     int i = 0;
     for (; my_str[i]; i++) {
         my_str[i] = toupper(my_str[i]);
     }
-    return my_str;
 }
